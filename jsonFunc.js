ccda543766c02238956b17241bb843d2cd6e4265
Songsungeun <korea.sesong@gmail.com>
2020-02-04 17:08:39 +0900

    비교대상과 동일한 값인 경우 false리턴하도록 추가

--- jsonFunc.js
+++ jsonFunc.js
@@ -1,7 +1,19 @@
 // OriginJson > ModifiedJson 수정된 부분 찾는 함수
+/* author: Song sungeun,
+   func: get the modified data from target Json
+   params: original Json, comparison Json
+   return: modified data object(json Type) or false(if. same json)
+*/
 const compareJson = (originJson, modifiedJson) => {
-  let differencedValues = {};
-  let addValue = (key) => { differencedValues[key] = modifiedJson[key]; }
+  let differencedValues;
+  let addValue = (key, nDepthValues) => {
+    let insertValue = nDepthValues || modifiedJson[key];
+
+    if (!insertValue) return;
+
+    differencedValues = differencedValues || {};
+    differencedValues[key] = insertValue;
+  };
 
   for (let key in modifiedJson) {
     // new key ? insert : next
@@ -15,16 +27,15 @@ const compareJson = (originJson, modifiedJson) => {
 
       try {
         if (originJson[key].constructor === Object && modifiedJson[key].constructor === Object) {
-          differencedValues[key] = compareJson(originJson[key], modifiedJson[key]);
+          let nDepthValues = compareJson(originJson[key], modifiedJson[key]);
+          nDepthValues && addValue(key, nDepthValues);
           continue;
         }
-        // object가 아닌 type들 insert (ex => Array, Number, String, etc.)
-        addValue(key);
+        addValue(key); // object가 아닌 type들 insert (ex => Array, Number, String, etc.)
       } catch (e) {
-        // constructor 없어서 오류나면 insert (ex => value > null, undefined > value, etc.)
-        addValue(key);
+        addValue(key); // constructor 없어서 오류나면 insert (ex => value > null, undefined > value, etc.)
       }
     }
   }
-  return differencedValues;
+  return differencedValues ? differencedValues : false;
 };
